fcc3562e3f6348aa47200fc51d13652fb7df3f84
quantruong158 <quantruong150803@gmail.com>
2025-04-05 15:06:15 +0700

    Add mock patients

--- Backend/src/Services/Patient/Patient.Api/Program.cs
+++ Backend/src/Services/Patient/Patient.Api/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.OpenApi.Models;
+using Patients.Api.Domain.Entities.Patients;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,56 @@ var builder = WebApplication.CreateBuilder(args);
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "Patient API", Version = "v1" });
     });
 }
+
+List<Patient> fakePatients =
+[
+    new Patient
+    {
+        Id = new Guid("A2559B6B-0CA9-4D88-90B8-9565386339C0"),
+        FirstName = "Minh",
+        LastName = "Nguyen",
+        PhoneNumber = "0912345678",
+        Email = "minh.nguyen@gmail.com",
+        DateOfBirth = new DateTime(1990, 5, 15)
+    },
+    new Patient
+    {
+        Id = new Guid("BA3BD31B-CF68-471D-B336-D4485FFF6BD7"),
+        FirstName = "Thi",
+        LastName = "Tran",
+        PhoneNumber = "0987654321",
+        Email = "thi.tran@yahoo.com",
+        DateOfBirth = new DateTime(1985, 8, 22)
+    },
+    new Patient
+    {
+        Id = new Guid("3E662D8D-5250-441F-B764-D9EFA0351492"),
+        FirstName = "Hoang",
+        LastName = "Pham",
+        PhoneNumber = "0909123456",
+        Email = "hoang.pham@outlook.com",
+        DateOfBirth = new DateTime(1995, 3, 10)
+    },
+    new Patient
+    {
+        Id = new Guid("07F448CA-50B9-406C-B584-B6107822EFBF"),
+        FirstName = "Linh",
+        LastName = "Le",
+        PhoneNumber = "0932145678",
+        Email = "linh.le@gmail.com",
+        DateOfBirth = new DateTime(1988, 12, 5)
+    },
+    new Patient
+    {
+        Id = new Guid("24645F44-8594-41E1-89D1-E22EBFD48522"),
+        FirstName = "Duc",
+        LastName = "Vo",
+        PhoneNumber = "0978123456",
+        Email = "duc.vo@hotmail.com",
+        DateOfBirth = new DateTime(1992, 7, 18)
+    }
+];
+
 
 var app = builder.Build();
 
@@ -21,6 +72,9 @@ if (app.Environment.IsDevelopment())
     });
 }
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/api/patients", () =>
+{
+    return Results.Ok(fakePatients);
+});
 
 app.Run();
